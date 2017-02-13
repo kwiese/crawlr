3161f06acdbc4c9b3229ccfeeaf5d553f414f856
Kyle Wiese <kwiese999@gmail.com>
2017-02-13 16:58:20 -0700

    segmenting js

---
+++ crawlrProject/static/js/display.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+  $('#pathdata').on('submit', function(e) {
+    e.preventDefault();
+    document.getElementById("queryPage").style.display = 'none';
+    document.getElementById("loading").style.display = 'block';
+    document.getElementById('timestamp').value = Date(Date.UTC());
+    $.ajax({
+      type: 'post',
+      data: $('#pathdata').serialize(),
+      dataType: 'json',
+      success: function(data) {
+        var ok = false;
+        document.getElementById("loading").style.display = 'none';
+        document.getElementById("results").style.display = 'block';
+        google.maps.event.trigger(map, 'resize');
+        $.each(data, function(k, v) {
+          if (k == "path"){
+            if (data[k].length > 0) {
+              ok = true;
+              for (var i in data[k]) {
+                var j = 65 + (parseInt(i));
+                var sss = String.fromCharCode(j);
+                $("#finalpath").append("<li class='list-group-item' style='margin: 5px'>"+ sss + ") "  + data[k][i] + "</li>");
+              }
+            } else {
+              document.getElementById("results").style.display = 'none';
+              document.getElementById("nopath").style.display = 'block';
+            }
+          } else if (k == "addresses"){
+            if (data[k].length > 0) {
+              ok = true;
+              var bounds = new google.maps.LatLngBounds();
+              var numCalls = data[k].length;
+              var waypts = [];
+              var o = null;
+              var d = null;
+
+              for (var i in data[k]) {
+                var entry = data[k][i];
+
+                if (i == 0) {
+                  o = entry;
+                } else if (i == data[k].length - 1) {
+                  d = entry;
+                } else {
+                  waypts.push({
+                    location: entry,
+                    stopover: true
+                  });
+                }
+              }
+              directionsService.route({
+                origin: o,
+                destination: d,
+                waypoints: waypts,
+                optimizeWaypoints: false,
+                travelMode: 'WALKING',
+              }, function(response, status) {
+                if (status == 'OK') {
+                  directionsDisplay.setDirections(response);
+                } else {
+                  alert('Directions request failed: ' + status);
+                }
+              });
+            }
+          }
+        });
+        if (ok == false) {
+          document.getElementById("results").style.display = 'none';
+          document.getElementById("nopath").style.display = 'block';
+        }
+      }
+    });
+  });
+});
