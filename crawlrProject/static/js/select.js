60e6e8e4749b365463688b7a1fec999e7d49ad39
Kyle Wiese <kwiese999@gmail.com>
2017-02-14 08:24:03 -0700

    js stuff 2

--- crawlrProject/static/js/select.js
+++ crawlrProject/static/js/select.js
@@ -34,28 +34,30 @@ var num_chosen = 0;
 
 function addKeywordSelect(){
   var entry = "<div class='form-group' id='keyword-" + num_chosen.toString() +"'>";
-  entry += "<select class='form-control' name='k-" + num_chosen.toString() + "'>";
+  entry += "<select class='form-control' name='k-" + num_chosen.toString() + "' onchange='displayOptions();'>";
   for (var key in chosen){
     if (chosen[key] == false){
       entry += "<option value='" + key + "'>" + key + "</option>";
     }
   }
   entry += "</select>";
+  entry += "<div class='form-group' id='keyword-options-" + num_chosen.toString() +"'> </div>";
   if (num_chosen < 3){
-    alert("In if!")
-    entry += "<button type='primary' name='add-" + num_chosen.toString() + "-options' onClick='displayOptions()'> Add another! </button>";
+    entry += "<button class='btn btn-primary' name='add-" + num_chosen.toString() + "-options' onClick='num_chosen += 1; addKeywordSelect();'> Add another! </button>";
   }
   entry += "</div>";
-  num_chosen += 1;
   document.getElementById("keyword-selection").innerHTML = document.getElementById("keyword-selection").innerHTML + entry;
 }
 
+
 function displayOptions(){
-  var outer_div = document.getElementById("keyword-"+num_chosen.toString());
+  var outer_div = document.getElementById("keyword-options-"+num_chosen.toString());
   var sel = document.getElementById("k-" + num_chosen.toString());
   var key = sel.options[sel.selectedIndex].value;
   var entry = "<h5 style='display:inline'>Don't spend longer than</h5><br>";
   entry += "<select name='" + key + "class='form-control' style='display:inline; width:10%; margin: 5px'>";
-  entry += ""
+  entry += "<option value=3600>1</option>";
+  entry += "</select>";
+  outer_div.innerHTML = outer_div.innerHTML + entry;
 
 }
