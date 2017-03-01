14e86ed5bba05d625f2a5fe9b2121833362028df
seanDoJo <sedo8743@colorado.edu>
2017-03-01 10:18:59 -0700

    faq updates

--- crawlrProject/crawlrProject/urls.py
+++ crawlrProject/crawlrProject/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^application/', include('crawlr.urls')), 
     url(r'^$', TemplateView.as_view(template_name='index_home.html')),
     url(r'^FAQ/', TemplateView.as_view(template_name='FAQ.html')),
+    url(r'^about/', TemplateView.as_view(template_name='about.html')),
 ]
 #urlpatterns = [url(r'^application/', TemplateView.as_view(template_name='index.html')), url(r'^$', TemplateView.as_view(template_name='index_home.html'))]
