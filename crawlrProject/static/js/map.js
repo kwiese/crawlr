7624b8d206b11edd2223c2a8228a99949ee87c64
EC2 Default User <ec2-user@ip-172-31-7-241.us-west-2.compute.internal>
2017-03-05 19:57:29 +0000

    michael's updates

---
+++ crawlrProject/static/js/map.js
@@ -0,0 +1,18 @@
+var map = null;
+  var start = null;
+  var geocoder = null;
+  var directionsService = null;
+  var directionsDisplay = null;
+
+function initMap() {
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 9,
+    center: {lat: 41.85, lng: -87.65},
+  });
+  directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsDisplay.setMap(map);
+  directionsService = new google.maps.DirectionsService;
+  var iA = document.getElementById('addr');
+  var autocomplete = new google.maps.places.Autocomplete(iA);
+}
