6d3d2b2208b5e7ec2fe58d84340dd3763af32bdc
seanDoJo <sedo8743@colorado.edu>
2017-03-10 09:56:53 -0700

    better error messages

--- crawlrProject/calculate.py
+++ crawlrProject/calculate.py
@@ -16,16 +16,45 @@ def start_chain(data):
             kname = "{}-selected".format(keyword)
             if kname in data:
                 keywords.append(keyword)
+
         d["keywords"] = keywords
+        if not len(keywords):
+            edata = {"path": [], "addresses": [], "error": "You didn't specify the kinds of places you want to visit!"}
+            return edata
 
         d["start_address"] = data["start_address"]
+        if not len(data["start_address"].strip()):
+            edata = {"path": [], "addresses": [], "error": "You didn't specify a start address!"}
+            return edata
 
-        d["radius"] = int(data["searchRadius"])
+        try:
+            d["radius"] = int(data["searchRadius"])
+            if (d["radius"] is not 805) or (d["radius"] is not 1609) or (d["radius"] is not 2414):
+                edata = {"path": [], "addresses": [], "error": "The search radius is an invalid value!"}
+                return edata
+        except Exception as e:
+            edata = {"path": [], "addresses": [], "error": "The search radius is an invalid value!"}
+            return edata
 
-        d["budget"] = int(data["budget"])
+        try:
+            d["budget"] = int(data["budget"])
+            if d["budget"] < 0 or d["budget"] > 4:
+                edata = {"path": [], "addresses": [], "error": "The budget is an invalid value!"}
+                return edata
+        except Exception as e:
+            edata = {"path": [], "addresses": [], "error": "The budget is an invalid value!"}
+            return edata
 
-        hour = int(data["userHour"])
-        minute = int(data["userMinute"])
+        try:
+            hour = int(data["userHour"])
+        except Exception as e:
+            edata = {"path": [], "addresses": [], "error": "Invalid value for your hours available!"}
+            return edata
+        try:
+            minute = int(data["userMinute"])
+        except Exception as e:
+            edata = {"path": [], "addresses": [], "error": "Invalid value for your minutes available!"}
+            return edata
 
         d["time"] = (hour + minute)
 
@@ -33,21 +62,30 @@ def start_chain(data):
         strictness = {}
         bounds = {"HOME": 0}
         for keyword in keywords:
-            kname = "{}-multiplier".format(keyword)
-            weights[keyword] = int(data[kname])
+            weights[keyword] = 1
 
             kname = "{}-equality".format(keyword)
             equality = data[kname]
             if equality != "NONE":
                 kname = "{}-strictness".format(keyword)
-                value = int(data[kname])
+                try:
+                    value = int(data[kname])
+                except Exception as e:
+                    edata = {"path": [], "addresses": [], "error": "Invalid equality value for {}!".format(keyword)}
+                    return edata
 
                 strictness[keyword] = (equality, value)
 
             kname = "{}-upperHour".format(keyword)
-            upperHour = int(data[kname])
+            try:
+                upperHour = int(data[kname])
+            except Exception as e:
+                upperHour = 0
             kname = "{}-upperMinute".format(keyword)
-            upperMinute = int(data[kname])
+            try:
+                upperMinute = int(data[kname])
+            except Exception as e:
+                upperMinute = 0
             bounds[keyword] = (upperHour + upperMinute)
 
         d["weights"] = weights
@@ -57,10 +95,26 @@ def start_chain(data):
         log(d)
         t = time.time()
         data = collectData(d)
+        if "error" in data:
+            edata = {"path": [], "addresses": [], "error": data["error"]}
+            return edata
+        placenum = 0
+        for k in strictness:
+            eq, va = strictness[k]
+            if eq == "EQ" and va is not len(data["placeData"][k]):
+                edata = {"path": [], "addresses": [], "error": "You said you wanted exactly {} {}'s, and we only found {}".format(va, k, len(data["placeData"][k])}
+                return edata
+            elif eq == "GTE" and va < len(data["placeData"][k]):
+                edata = {"path": [], "addresses": [], "error": "You said you wanted at least {} {}'s, and we only found {}".format(va, k, len(data["placeData"][k])}
+                return edata
+            placenum += len(data["placeData"][k])
+        if placenum == 0:
+            edata = {"path": [], "addresses": [], "error": "We couldn't find any of the places you specified!"}
+            return edata
         log("total data collection {}".format((time.time() - t)))
     except Exception as e:
         log(traceback.format_exc())
-        edata = {"path": [], "addresses": []}
+        edata = {"path": [], "addresses": [], "error": "Something went wrong when we tried getting your data!"}
         return edata
     try:
         log("starting solving")
